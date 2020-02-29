0f041949f9b83c4b2b74ffad987ce24ba7815ee4
BalazsBence <43274435+BalazsBence@users.noreply.github.com>
2020-02-29 23:35:39 +0100

    Update csere.cpp

--- csere.cpp
+++ csere.cpp
@@ -1,21 +1,27 @@
 #include "std_lib_facilities.h"
 
-int main()
+int main(){
+int a=5;
+int b=6;
+int c;
 {
-int a,b;
-
       a=a+b;
       b=a-b;
       a=a-b;
 
 cout<<"a="<<a<<'\n'<<"b="<<b<<'\n';
- int c 
+}
+{
       c=a;
       a=b;
       b=c;
 cout<<"a="<<a<<'\n'<<"b="<<b<<'\n';
+}
+{
       a=a^b;
       b=a^b;
       a=a^b;
 cout<<"a="<<a<<'\n'<<"b="<<b<<'\n';
+}
+
 }
